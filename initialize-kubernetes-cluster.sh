2ecdfbdc4bf314453bc1240f9fe84b391a395ad0
Javier Cristobal <javier.cristobal.toscano@gmail.com>
2025-08-07 13:50:09 +0200

    Hello World Flask V1

--- initialize-kubernetes-cluster.sh
+++ initialize-kubernetes-cluster.sh
@@ -3,8 +3,8 @@
 kubeadm init --apiserver-advertise-address $(hostname -i)
 
 mkdir -p $HOME/.kube
-sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
-sudo chown $(id -u):$(id -g) $HOME/.kube/config
+cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+chown $(id -u):$(id -g) $HOME/.kube/config
 
 kubectl apply -f https://raw.githubusercontent.com/cloudnativelabs/kube-router/master/daemonset/kubeadm-kuberouter.yaml
 
